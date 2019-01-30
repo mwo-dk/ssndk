79529eedac13cd5370fcdd6c0b94f9e881921661
Michael Wiinberg Olesen <michael.w.olesen@outlook.com>
2019-01-30 20:46:41 +0100

    Added isValid and extensions

---
+++ DotNet/SSNDK.CSharp.Test/IsValidTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+using FsCheck;
+using FsCheck.Xunit;
+using SSNDKCS;
+using static SSNDKCSharp.Test.TestHelpers;
+
+namespace SSNDKCSharp.Test
+{
+    public class IsValidTest
+    {
+        [Property]
+        [Trait("Category", "Unit")]
+        public Property IsValid_with_non_digits_work(DateTimeOffset x,
+            bool dash, NonNegativeInt controlCode, bool useModula11Check)
+        {
+            var sut = GetSSNWithNoneDigits(x, dash, controlCode);
+
+            return sut.IsValid(useModula11Check).ToProperty();
+        }
+
+        [Property]
+        [Trait("Category", "Unit")]
+        public Property IsValid_with_dash_failure_work(DateTimeOffset x,
+            NonNegativeInt controlCode, bool useModula11Check)
+        {
+            var sut = GetSSNWithDashFailure(x, controlCode);
+
+            return (!sut.IsValid(useModula11Check)).ToProperty();
+        }
+    }
+}
